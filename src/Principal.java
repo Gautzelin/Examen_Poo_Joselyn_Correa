9d0c341693ef5ec5ce908d27d477032f1d03dd66
Joss <joselyn.correa@epn.edu.ec>
2022-07-11 07:47:51 -0500

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Principal {
+    protected int retiros, depositos, transacciones;
+    private int saldo;
+    Scanner entrada = new Scanner(System.in);
+    public void Operaciones(){
+        int n1=0;
+        int seleccion =0;
+    }
+}
